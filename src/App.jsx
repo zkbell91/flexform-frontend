14af8f8043daa05acfb546d23015af93c9aa9634
Zachary Bell <z.bell@me.com>
2024-06-28 22:59:45 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <p>Welcome to FlexForm</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>FlexForm</h1>
+  </header>
+);
+
+export default Header;
